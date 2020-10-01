cae9f0c56a4b94de2c3e37d5da89a475c9dea06b
Matias Gutierrez <gutierrez.matias@outlook.com>
2020-10-01 18:29:22 -0300

    add envs to react

--- front/src/features/session/sessionSlice.js
+++ front/src/features/session/sessionSlice.js
@@ -1,7 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import socketIOClient from "socket.io-client";
 import axios from 'axios';
-
 
 const localStorageSessionKey = 'session';
 
@@ -25,23 +23,7 @@ export const sessionSlice = createSlice({
 export const { login, destroySession } = sessionSlice.actions;
 
 export const initSession = () => dispatch => {
-  // const socket = socketIOClient('https://localhost:5000');
-  // const clientId = guid();
-  //
-  // socket.on('connect', function () {
-  //   socket.emit('client::user::connected', {id: clientId});
-  //
-  //   socket.on(`server::redirect::${clientId}`, function (redirectUrl) {
-  //     window.open(redirectUrl);
-  //   });
-  //
-  //   socket.on(`server::user::logged_in::${clientId}`, (payload) => {
-  //     localStorage.setItem(localStorageSessionKey, JSON.stringify(payload));
-  //     dispatch(login(payload));
-  //     socket.close();
-  //   })
-  // });
-  axios.post(`http://localhost:5000/api/sessions`, {username: 'admin', password: 'Passw0rd!'}).then(res => {
+  axios.post(`${process.env.REACT_APP_API}/api/sessions`, {username: 'admin', password: 'Passw0rd!'}).then(res => {
     const user = {
       token: res.data.token,
       name: 'admin'
@@ -50,13 +32,6 @@ export const initSession = () => dispatch => {
     dispatch(login(user))
   })
 };
-
-function guid() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,c => {
-    const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r && 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
 
 export const selectSession = state => state.session;
 
